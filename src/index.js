825070a6cea7191efcfec354a24b9a5af4561c39
Naresh goud <ediganareshgoud406@gmail.com>
2025-02-13 16:24:37 +0530

    Cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { useState } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
